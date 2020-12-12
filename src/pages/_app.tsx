dc4c14a49b99d989efedd46a80ef2194fb1eb72e
ViniciusCrisol <vinicius.crisol@hotmail.com>
2020-12-12 19:41:00 -0300

    Revert GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,11 @@
-import { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
-
-import { initGA, logPageView } from '../lib/analytics'
 
 import theme from '../styles/theme'
 import GlobalStyles from '../styles/global'
 import Header from '../components/Header'
 
-declare global {
-  interface Window {
-    GA_INITIALIZED: any
-  }
-}
-
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  useEffect(() => {
-    if (!window.GA_INITIALIZED) {
-      initGA()
-      window.GA_INITIALIZED = true
-    }
-    logPageView()
-  }, [])
-
   return (
     <ThemeProvider theme={theme}>
       <Header />
